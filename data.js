5270326f775bd2c4dda0397e929a72a0a2559503
gvasilio <guilhermevasilio@gmail.com>
2021-05-10 11:07:07 -0300

    Ap 21 Terramerica Americana

---
+++ data.js
@@ -0,0 +1,495 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-varanda",
+      "name": "Varanda",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": 0.14024899038941285,
+        "pitch": 0.10955966246034698,
+        "fov": 1.4881854150864144
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0821546576367282,
+          "pitch": 0.567329517261161,
+          "rotation": 0,
+          "target": "1-sala-de-tv"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-sala-de-tv",
+      "name": "Sala de TV",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": 0.12366129986422614,
+        "pitch": -0.04041252363843029,
+        "fov": 1.4881854150864144
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3148909989852235,
+          "pitch": 0.32188136314792004,
+          "rotation": 0,
+          "target": "3-lavanderia"
+        },
+        {
+          "yaw": -0.34055287805815837,
+          "pitch": 0.3367731266080227,
+          "rotation": 0,
+          "target": "4-corredor"
+        },
+        {
+          "yaw": 2.3455373234866235,
+          "pitch": 0.26986190571189184,
+          "rotation": 0,
+          "target": "0-varanda"
+        },
+        {
+          "yaw": 2.923212402429776,
+          "pitch": 0.2879957308365384,
+          "rotation": 0,
+          "target": "2-cozinha"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-cozinha",
+      "name": "Cozinha",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": 2.1497983755142966,
+        "pitch": 0.08739527387202273,
+        "fov": 1.4881854150864144
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.560855039306432,
+          "pitch": 0.4267182683492763,
+          "rotation": 0,
+          "target": "3-lavanderia"
+        },
+        {
+          "yaw": 0.16201385131043544,
+          "pitch": 0.39097994555505444,
+          "rotation": 0,
+          "target": "1-sala-de-tv"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-lavanderia",
+      "name": "Lavanderia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -1.9290231961743238,
+        "pitch": 0.07196127794125573,
+        "fov": 1.4881854150864144
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1249609976020842,
+          "pitch": 0.3470726978921519,
+          "rotation": 0,
+          "target": "2-cozinha"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-corredor",
+      "name": "Corredor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.059125183791833,
+          "pitch": 0.31251131152922973,
+          "rotation": 0,
+          "target": "1-sala-de-tv"
+        },
+        {
+          "yaw": -0.228528933306114,
+          "pitch": 0.4550675377280804,
+          "rotation": 0,
+          "target": "5-quarto-1"
+        },
+        {
+          "yaw": -0.12362224893603901,
+          "pitch": 0.29047264819507923,
+          "rotation": 0,
+          "target": "6-banheiro-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-quarto-1",
+      "name": "Quarto 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": 0.060652902496729055,
+        "pitch": -0.10955966246034166,
+        "fov": 1.4881854150864144
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2213567973410946,
+          "pitch": 0.35382442468850783,
+          "rotation": 0,
+          "target": "7-corredor-continuao"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-banheiro-1",
+      "name": "Banheiro 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": 1.1699167258925662,
+        "pitch": -0.013481227825570485,
+        "fov": 1.4881854150864144
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.17880565274668925,
+          "pitch": 0.4028098076157711,
+          "rotation": 0,
+          "target": "7-corredor-continuao"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-corredor-continuao",
+      "name": "Corredor Continuação",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0739388548899136,
+          "pitch": 0.0949610092918114,
+          "rotation": 0,
+          "target": "1-sala-de-tv"
+        },
+        {
+          "yaw": -2.8604256662933434,
+          "pitch": 0.36684393143493743,
+          "rotation": 0,
+          "target": "5-quarto-1"
+        },
+        {
+          "yaw": -2.3968126026172154,
+          "pitch": 0.5385164797682265,
+          "rotation": 0,
+          "target": "6-banheiro-1"
+        },
+        {
+          "yaw": -1.432257450794939,
+          "pitch": 0.5092854462440002,
+          "rotation": 0,
+          "target": "7-corredor-continuao"
+        },
+        {
+          "yaw": 0.024649933727914686,
+          "pitch": 0.36572508903529766,
+          "rotation": 0,
+          "target": "9-quarto-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-quarto-2",
+      "name": "Quarto 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -2.902757205746859,
+        "pitch": -0.0745275811112247,
+        "fov": 1.4881854150864144
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8663747786537606,
+          "pitch": 0.39747708275422866,
+          "rotation": 0,
+          "target": "7-corredor-continuao"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-quarto-3",
+      "name": "Quarto 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": 0.43319760272242647,
+        "pitch": 0.060647506419860164,
+        "fov": 1.4881854150864144
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.433197870834821,
+          "pitch": 0.15450424105861593,
+          "rotation": 0,
+          "target": "10-banheiro-2"
+        },
+        {
+          "yaw": -1.750559422273108,
+          "pitch": 0.293649226529185,
+          "rotation": 0,
+          "target": "7-corredor-continuao"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-banheiro-2",
+      "name": "Banheiro 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": 2.4768583179667676,
+        "pitch": -0.07253831314015358,
+        "fov": 1.4881854150864144
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.04312640872903728,
+          "pitch": 0.4802217013507537,
+          "rotation": 0,
+          "target": "9-quarto-3"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Ap 21 Terramerica - Americana",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
